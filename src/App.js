4a643a33c33d3e06a79365515e954ba3b4d98b9d
Greg Pfaff <greg.pfaff@silvercar.com>
2020-09-20 17:14:00 -0500

    change name

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-         Barstool Tech
+         Barstool Sports Tech
       </header>
       <div role="main">
         <Wrapper />
